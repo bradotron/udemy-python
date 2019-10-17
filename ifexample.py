24e766bf3aacd6f20ff2e2ddc64556e835befc95
Bradley Jensen <bradley13@gmail.com>
2019-10-17 10:12:48 -0400

    lesson one completed

---
+++ ifexample.py
@@ -0,0 +1,7 @@
+print('Enter password:')
+password = input()
+
+if password == 'swordfish':
+  print('access granted')
+else:
+  print('access denied, prepare to be deleted')
---
+++ lesson-one.py
@@ -0,0 +1,12 @@
+# This program say hello and asks for my name
+
+print('Hello World!')
+print('What is your name?')
+myName = input()
+print('It is good to me you, ' + myName)
+print('Your name is ' + str(int(len(myName))) + ' characters long.')
+print(len(myName))
+
+print('How old are you?')
+myAge = input()
+print('You will be ' + str(int(myAge) + 1) + ' in a year.' )
