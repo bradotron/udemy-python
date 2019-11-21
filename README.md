39e13d968787f56e2a440381f4446aa786d8b46c
Bradley Jensen <bradley13@gmail.com>
2019-11-21 18:27:58 -0500

    for loops added

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # udemy-python
 Repo for Automate the Boring Stuff with Python
+
+while syntax
+...
+while true:
+  print('I'm doing stuff')
+  break
+...
+break ends your loop without checking the condition again
--- while-example.py
+++ while-example.py
@@ -3,8 +3,15 @@ while spam < 5:
   print('Hello World! Sexy Butts')
   spam = spam + 1
 
-name = ''
-while name != 'your name':
-  print('Please type your name.')
-  name = input()
-print('Thank you')
+# name = ''
+# while name != 'your name':
+#   print('Please type your name.')
+#   name = input()
+# print('Thank you')
+
+foo = 0
+while foo < 5:
+  foo += 1
+  if spam == 3:
+    continue
+  print('spam is ' + str(foo))
